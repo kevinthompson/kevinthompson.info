dab5f6bda005cc8c91e5928f82fd1a3c88028728
Kevin Thompson <email@kevinthompson.info>
2024-06-20 09:46:01 -0700

    chore: make blog header image optional

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,14 +4,17 @@ const blogCollection = defineCollection({
   schema: ({ image }) =>
     z.object({
       title: z.string(),
-      image: z.object({
-        src: image(),
-        alt: z.string(),
-      }),
+      image: z
+        .object({
+          src: image(),
+          alt: z.string(),
+        })
+        .optional(),
       category: z.enum(['Stories', 'Tutorials']),
       description: z.string(),
       publishDate: z.date(),
       canonicalURL: z.string().optional(),
+      showCoverImage: z.boolean().default(true),
     }),
 })
 
