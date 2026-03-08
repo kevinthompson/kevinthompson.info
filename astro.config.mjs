d858a874cdf05b6ae8d98dcaabbaea191438098b
Kevin Thompson <email@kevinthompson.info>
2026-03-08 12:00:06 -0700

    fix: restore trailingSlash: never to match existing sitemap URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
   site: 'https://kevinthompson.info',
+  trailingSlash: 'never',
   integrations: [mdx(), sitemap(), tailwind()],
 })
