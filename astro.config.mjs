01ec93c9351d607fc532ddaee664e2f63b3eb2ab
Kevin Thompson <email@kevinthompson.info>
2026-03-08 18:16:14 -0700

    feat: configure Shiki syntax highlighting themes
    Use github-light for light mode and tokyo-night for dark mode.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://kevinthompson.info',
-  trailingSlash: 'never',
+  site: "https://kevinthompson.info",
+  trailingSlash: "never",
   integrations: [mdx(), sitemap(), tailwind()],
-})
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: "tokyo-night",
+      },
+    },
+  },
+});
