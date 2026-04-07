b5059664268f6c59d97db3ee3d85f8eb04f27193
Kevin Thompson <email@kevinthompson.info>
2026-04-07 07:18:33 -0700

    chore: update youtube account name

--- src/config.ts
+++ src/config.ts
@@ -61,7 +61,7 @@ export const SOCIALS: SocialObjects = [
   },
   {
     name: 'youTube',
-    href: 'https://youtube.com/@kevin.makes.videos',
+    href: 'https://youtube.com/@kevinmakesgames',
     label: 'YouTube',
     ariaLabel: 'Follow on YouTube',
     showInHero: true,
